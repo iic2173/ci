bfdc935a3ea27e9f63e68921217c4299ec4a3ccc
Borja MPM <bmarquezdelaplata@uc.cl>
2024-03-24 17:57:40 -0300

    arreglo linter

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ const express = require("express");
 
 // Create an instance of Express
 const app = express();
-const a = 0
 // Define the GET / route
 app.get("/", (req, res) => {
   res.send("Hello, World!!!"); // Send a response to the client
